1cdae9b1d42c7ce69c0428ed86ccb353f0a8080c
Itsystemsproyect <92403019+Itsystemsproyect@users.noreply.github.com>
2021-10-21 23:36:42 -0500

    css login

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Form, Input, Button } from "antd";
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import itsc from "../assets/img/usuario.png";
+import "../assets/css/LoginPage.css"
 
 const LoginPage = () => {
   return (
