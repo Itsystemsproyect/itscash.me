9b29486da0301e3f24527e79131fd03bf75d786a
kevin <knuez@itsystems.ai>
2021-10-26 15:41:15 -0500

    Acerca de

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,6 +6,9 @@ import Navbar from "../components/Navbar";
 import itsc from "../assets/img/itscx400.svg";
 import Particulas from "../components/particulas/Particulas";
 import '../assets/css/Particulas.css'
+import imgmision from "../assets/img/mision.png";
+import imgvision from "../assets/img/vision.png";
+import imgobjetivos from "../assets/img/valores.png";
 
 const { Content, Footer } = Layout;
 
@@ -15,14 +18,16 @@ const Home = () => {
       <Layout className="layout">
         <Navbar />
         <Content className="banner0">
-              <div id='parti'  style={{ position: 'absolute'}}>
-              <Particulas height="100vh" width="100%"/>
-              </div> 
+          <div id="parti" style={{ position: "absolute" }}>
+            <Particulas height="100vh" width="100%" />
+          </div>
           <div className="logobanner">
             <div className="imglogo">
-              <img src={itsc} alt="logo"/>
+              <img src={itsc} alt="logo" />
             </div>
-            <p className="tituloits">PROYECTO DE RECOMPENSAS ITSYSTEMS</p>
+            <p className="tituloits">
+              PROYECTO DE RECOMPENSAS POR PRODUCTIVIDAD LABORAL
+            </p>
             <div className="botones">
               <Link to="/login">
                 <Button className="btn1" ghost key="boton1">
@@ -35,26 +40,36 @@ const Home = () => {
             </div>
           </div>
         </Content>
-        <div className="transactionstatic">
-        </div>
-        <Content className="content2"> 
+        <div className="transactionstatic"></div>
+        <Content className="content2">
           <div className="descripcion">ACERCA DE </div>
           <div className="contenedor2">
-            <div className="mision">
+            <div className="mision info">
+              <img className="misimg" src={imgmision} />
+              <p className="parrasc">
+                Recompensar por el esfuerzo que realizan los trabajadores para
+                sacar adelante una Empresa, cumpliendo con los objetivos y metas
+                Trasadas
+              </p>
               MISION
             </div>
-            <div className="vision">
+            <div className="vision info">
               VISION
+              <p className="parrasc">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin vitae lorem quis ultricies. Quisque nec imperdiet ex. Ut pulvinar eu mi quis efficitur.
+              </p>
+              <img className="misimg" src={imgvision} />
             </div>
-            <div className="objetivos">
-              OBJETIVOS
+            <div className="objetivos info">
+              <img className="misimg" src={imgobjetivos} />
+              <p className="parrasc"></p>
+              VALORES
             </div>
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
           ITSYSTEMS ©2021 Todos Los Derechos Reservados.
         </Footer>
-
       </Layout>
     </>
   );
