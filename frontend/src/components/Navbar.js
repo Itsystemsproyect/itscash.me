208a084b09ee6b8aa74e748aaa97f6110c2cc0a9
Fbossio <felixbossio@gmail.com>
2021-11-04 12:58:42 -0500

    Cambiar la navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,25 @@
-import React, { useContext } from "react";
-import { Layout, Menu, Button } from "antd";
-import { SubMenu } from "rc-menu";
-import { Link } from "react-router-dom";
-import itsc from "../assets/img/itscx400.svg";
+import React, { useContext, useState } from "react";
 import AuthContext from "../context/auth/authContext";
+import { NavLink } from "react-router-dom";
+import { Button } from "antd";
+import itsc from "../assets/img/itscx400.svg";
+import '../assets/css/Navbar.css'
 
-const { Header } = Layout;
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
   const authContext = useContext(AuthContext);
 
   const { isAuthenticated, logout, user } = authContext;
+
+  const handleClick = () => setClick(!click);
 
   const handleLogout = () => {
     logout();
   };
 
+<<<<<<< HEAD
   const authLinks = (
     <>
       <Header>
@@ -52,11 +56,25 @@ const Navbar = () => {
               </Menu.Item>
             </SubMenu>
           </Menu>
+=======
+  const guestLinks = (<>
+    <nav className="navbar">
+      <div className="nav-container">
+        <NavLink exact to="/" className="nav-logo">
+        <div className="itslogo">
+          <img
+            className="itslogoimg"
+            src={itsc}
+            alt="img"
+            width={50}
+            height="auto"
+          />
+          <pre className="tituitsc"> ITSYSTEMS PROJECT</pre>
+>>>>>>> Cambiar la navbar
         </div>
-      </Header>
-    </>
-  );
+        </NavLink>
 
+<<<<<<< HEAD
   const guestLinks = (
     <>
       <Header>
@@ -75,21 +93,107 @@ const Navbar = () => {
             <Menu.Item key="1">
               <Link to="/">Home</Link>
             </Menu.Item>
-
-            <Menu.Item key="2">
-              <Link to="/register">Sign-Up</Link>
-            </Menu.Item>
-
-            <Menu.Item key="3">
-              <Link to="/login">Login</Link>
-            </Menu.Item>
-          </Menu>
+=======
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/login"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Login
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/register"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Regístrate
+            </NavLink>
+          </li>
+        
+        </ul>
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
-      </Header>
-    </>
-  );
+      </div>
+    </nav>
+  </>)
+>>>>>>> Cambiar la navbar
 
-  return <>{isAuthenticated ? authLinks : guestLinks}</>;
+  const authLinks = (<>
+    <nav className="navbar">
+      <div className="nav-container">
+        <NavLink exact to="/" className="nav-logo">
+        <div className="itslogo">
+          <img
+            className="itslogoimg"
+            src={itsc}
+            alt="img"
+            width={50}
+            height="auto"
+          />
+          <pre className="tituitsc"> ITSYSTEMS PROJECT</pre>
+        </div>
+        </NavLink>
+
+        <ul className={click ? "nav-menu active" : "nav-menu"}>       
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/profile"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Perfil
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/wallet"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Wallet
+            </NavLink>
+          </li>
+          <li className="nav-item">
+          <Button type="text" onClick={handleLogout} className="nav-links">
+              <span className="navButton">Log-Out</span>
+          </Button>
+          </li>
+        </ul>
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+      </div>
+    </nav>
+  </>)
+
+return <>{isAuthenticated ? authLinks : guestLinks}</>;
+
+  
 };
 
 export default Navbar;
